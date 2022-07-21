9069af4d93fa482ce81caa4e3efeceac23476507
nick <nikolai.roshchin@gmail.com>
2022-07-21 12:45:30 +0300

    Added validates and associations

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,10 @@ FactoryBot.define do
   factory :task do
     name { "MyString" }
     description { "MyText" }
-    author_id { 1 }
-    assignee_id { 1 }
+#   author_id  { 1} 
+    association :author, factory: :manager
+#    assignee_id { 1 }
+    association :assignee, factory: :developer
     state { "MyString" }
     expired_at { "2022-07-20" }
   end
