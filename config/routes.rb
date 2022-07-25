5df6a1e34256af2827f4355c827ce4929356fa92
nick <nikolai.roshchin@gmail.com>
2022-07-25 12:16:36 +0300

    Added developers controller and auth_helper

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   scope module: :web do
     resource :board, only: :show
-    resource :session, only: [ :new, :create, :destroy ]
+    resource :session, only: [:new, :create, :destroy]
+    resource :developers, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
