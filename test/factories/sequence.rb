70b06bd3ee8f4d35766e7aa37bf71456bbe0fa3d
nick <nikolai.roshchin@gmail.com>
2022-08-05 13:13:02 +0300

    Added tasks factories

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :name, :description] do |n|
     "string#{n}"
   end
 
   sequence :email do |n|
     "person#{n}@example.com"
+  end
+
+  sequence :state, %i[new_task, in_development, archived, in_qa, in_code_review, ready_for_release, released].cycle
+
+  sequence :expired_at do |n| 
+    Date.today + n.day
   end
 end
