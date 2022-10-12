1533f1a5c7cbe174c78f5eb198bed8c15bd03377
nick <nikolai.roshchin@gmail.com>
2022-10-12 21:09:57 +0300

    Added mailer template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,23 +2,23 @@ class UserMailer < ApplicationMailer
 
   def task_created
     user = params[:user]
-    @task = params[:task]
-
-    mail(from: 'noreply@taskmanager.com', to: user.email, subject: 'New Task Created')
+    task = params[:task]
+    @message = "Task # #{task.id} was created"
+    mail(from: 'noreply@taskmanager.com', to: user.email, subject: 'New Task Created', template_name: 'task_template')
   end
 
   def task_updated
     user = params[:user]
-    @task = params[:task]
-
-    mail(from: 'noreply@taskmanager.com', to: user.email, subject: 'Task Updated')
+    task = params[:task]
+    @message = "Task # #{task.id} was updated"
+    mail(from: 'noreply@taskmanager.com', to: user.email, subject: 'Task Updated', template_name: 'task_template')
   end
 
   def task_deleted
     user = params[:user]
-    @task = params[:task]
-
-    mail(from: 'noreply@taskmanager.com', to: user.email, subject: 'Task Deleted')
+    task = params[:task]
+    @message = "Task # #{task.id} was deleted"
+    mail(from: 'noreply@taskmanager.com', to: user.email, subject: 'Task Deleted', template_name: 'task_template')
   end
 
 end
