d2309325428d715885447863e6ba71ce56ac474f
nick <nikolai.roshchin@gmail.com>
2022-10-14 09:50:09 +0300

    Changed access token

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-app_env = File.join(Rails.root, 'config', 'env_set.rb')
-load(app_env) if File.exist?(app_env)
-
 # Initialize the Rails application.
 Rails.application.initialize!
